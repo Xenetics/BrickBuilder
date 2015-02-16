e809f5be0f2a8b8de859236f4f4ba18425f8dfbf
AlienJim <burton.alexander102@gmail.com>
2015-02-15 20:06:31 -0500

    Simple Camera

---
+++ Assets/Scripts/General/CameraController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	//publics
+	public bool topDownMovmentEnabled = true;
+	//camera Speeds
+	public float speedTopDown = 1f;
+	public float speedShowcase = 1f;
+
+	public CameraStates DefaultMode = CameraStates.TopDown;
+
+	public enum CameraStates{TopDown, Showcase, Transition, FirstPerson};
+	//for transitions
+	private Vector3 targetPos;
+	private Vector3 targetDir;
+	//first person target, showcase target...
+	private Transform targetFP;
+	private Transform targetSC;
+
+	private CameraStates cState;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(cState)
+		{
+		case CameraStates.TopDown:
+			if(topDownMovmentEnabled)
+			{
+				if(Input.mousePosition.x < 3)
+				{
+					Vector3 temp = transform.position;
+					//temp.z -= 1;
+					temp.x -= speedTopDown;
+					transform.position = temp;
+				}
+				else if(Input.mousePosition.y < 3)
+				{
+					Vector3 temp = transform.position;
+					//temp.z -= 1;
+					temp.y -= speedTopDown;
+					transform.position = temp;
+				}
+				else if(Input.mousePosition.x > Screen.width - 3)
+				{
+					Vector3 temp = transform.position;
+					//temp.z -= 1;
+					temp.x += speedTopDown;
+					transform.position = temp;
+				}
+				else if(Input.mousePosition.y > Screen.height - 3)
+				{
+					Vector3 temp = transform.position;
+					//temp.z -= 1;
+					temp.y += speedTopDown;
+					transform.position = temp;
+				}
+
+				if(Input.GetAxis("Mouse ScrollWheel") > 0.1)
+				{
+					Vector3 temp = transform.position;
+					temp.z += speedTopDown;
+					transform.position = temp;
+				}
+				else if(Input.GetAxis("Mouse ScrollWheel") < -0.1)
+				{
+					Vector3 temp = transform.position;
+					temp.z -= speedTopDown;
+					transform.position = temp;
+				}
+			}
+			break;
+		}
+	}
+}
