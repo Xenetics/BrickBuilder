22d0870018c09f18b276953c9bec462d32a53c9d
AlienJim <burton.alexander102@gmail.com>
2015-03-11 20:26:31 -0400

    more toast stuff

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+    public Transform Ball;
+    public float speed;
+
+    private Rigidbody body;
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    //if the ball is to the left then move left at speed
+        //if the ball is to the right then move right
+        //else if the ball is anywhere above the paddle then dont move (maybe some idle moving)
+	}
+
+    //on collision something just change the velocity to a fixed z and an x that 
+    //is based on how far the ball is from the center and then the ball will normailize its speed and go on its merry way
+}
---
+++ Assets/Scripts/WallBounce.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallBounce : MonoBehaviour {
+    public bool x;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Vector3 temp = other.GetComponent<Rigidbody>().velocity;
+        if(x == true)
+        {
+            temp.x *= -1;
+        }
+        else
+        {
+            temp.z *= -1;
+        }
+        other.GetComponent<Rigidbody>().velocity = temp;
+    }
+}
