7258e69bf12873ade9401d5390231fd6fb981429
AlienJim <burton.alexander102@gmail.com>
2015-03-16 17:25:44 -0400

    Paddle AI

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,21 +2,49 @@
 using System.Collections;
 
 public class Paddle : MonoBehaviour {
-    public Transform Ball;
+    public Transform ball;
     public float speed;
+    public float paddleWidth = 10f;
+    public float moveDistance = 10f;
 
+    //private CapsuleCollider col;
     private Rigidbody body;
 	// Use this for initialization
 	void Start () {
         body = GetComponent<Rigidbody>();
+        //col = GetComponent<CapsuleCollider>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	    //if the ball is to the left then move left at speed
-        //if the ball is to the right then move right
-        //else if the ball is anywhere above the paddle then dont move (maybe some idle moving)
+	void FixedUpdate () {
+        //only do this if the ball is moving towards the paddle and if it is a certain distance away
+        if(ball.GetComponent<Rigidbody>().velocity.z < 0 && Mathf.Abs(ball.position.z - transform.position.z ) < moveDistance)
+        {
+            if(ball.transform.position.x  <  transform.position.x - (0.4 * paddleWidth))
+            {
+                body.velocity = new Vector3(-speed, 0, 0);
+            }
+            else if (ball.transform.position.x > transform.position.x + (0.4 * paddleWidth))
+            {
+                body.velocity = new Vector3(speed, 0, 0);
+            }
+        }   
+        else
+        {
+            body.velocity = new Vector3(0, 0, 0);
+            //else if the ball is anywhere above the paddle then dont move (maybe some idle moving)
+        }        
 	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform == ball.transform)
+        {
+            Vector3 vel = new Vector3(ball.position.x - transform.position.x, 0, 2.0f);
+            vel.Normalize();
+            ball.GetComponent<Rigidbody>().velocity = vel;
+        }
+    }
 
     //on collision something just change the velocity to a fixed z and an x that 
     //is based on how far the ball is from the center and then the ball will normailize its speed and go on its merry way
