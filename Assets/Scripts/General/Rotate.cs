95e1a437e6a67f51f242fb6656edcc04312e3b0c
Xenetics <xenetics@live.com>
2015-03-20 16:55:41 -0400

    Menu Prototype

---
+++ Assets/Scripts/General/Rotate.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour 
+{
+    [SerializeField]
+    private GameObject entity;
+    [SerializeField]
+    private bool rotateX = false;
+    [SerializeField]
+    private bool rotateY = false;
+    [SerializeField]
+    private bool rotateZ = false;
+
+	void Start () 
+    {
+	    
+	}
+	
+	void Update () 
+    {
+        if (rotateX)
+        {
+            entity.transform.Rotate(Vector3.right);
+        }
+
+        if (rotateY)
+        {
+            entity.transform.Rotate(Vector3.up);
+        }
+
+        if (rotateZ)
+        {
+            entity.transform.Rotate(Vector3.forward);
+        }
+	}
+}
