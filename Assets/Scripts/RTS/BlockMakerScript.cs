d64dc2fb88d1969aea1c6934ca9a4b246a8ef17b
JJeewanjee <j.jeewanjee@gmail.com>
2015-03-27 15:25:12 -0400

    You can now build blocks!
    Select the first building and use the 1-5 keys to queue a block build in
    the specified column.

---
+++ Assets/Scripts/RTS/BlockMakerScript.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class BlockMakerScript : MonoBehaviour {
+
+	private BlockManager blockManager;
+
+	public GameObject[] blocks;
+	public float blockBuildTime;
+	public float timeRemaining;
+	private GameObject currentlyBuilding;
+	private int column;
+
+	public List<GameObject> blockQueue;
+
+	public GameObject selected;
+
+	// Use this for initialization
+	void Start () {
+		blockManager = GameObject.FindGameObjectWithTag ("BlockManager").GetComponent<BlockManager> ();
+		for(int i = 0; i < 3; i++)
+		{
+			for(int j = 0; j < 5; j++)
+			{
+				blockManager.Build(blocks[Random.Range(0, 5)], j);
+			}
+		}
+
+		for(int i = 0; i < 10; i++)
+		{
+			blockQueue.Add(blocks[Random.Range(0, 5)]);
+		}
+
+		timeRemaining = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(selected.activeSelf)
+		{
+			if (Input.GetKeyUp (KeyCode.Alpha1) && timeRemaining <= 0 && !blockManager.ColFull(0))
+			{
+				currentlyBuilding = blockQueue[0];
+				column = 0;
+				blockQueue.RemoveAt(0);
+				blockQueue.Add(blocks[Random.Range(0, 5)]);
+				timeRemaining = blockBuildTime;
+			}
+			if (Input.GetKeyUp (KeyCode.Alpha2) && timeRemaining <= 0 && !blockManager.ColFull(1))
+			{
+				currentlyBuilding = blockQueue[0];
+				column = 1;
+				blockQueue.RemoveAt(0);
+				blockQueue.Add(blocks[Random.Range(0, 5)]);
+				timeRemaining = blockBuildTime;
+			}
+			if (Input.GetKeyUp (KeyCode.Alpha3) && timeRemaining <= 0 && !blockManager.ColFull(2))
+			{
+				currentlyBuilding = blockQueue[0];
+				column = 2;
+				blockQueue.RemoveAt(0);
+				blockQueue.Add(blocks[Random.Range(0, 5)]);
+				timeRemaining = blockBuildTime;
+			}
+			if (Input.GetKeyUp (KeyCode.Alpha4) && timeRemaining <= 0 && !blockManager.ColFull(3))
+			{
+				currentlyBuilding = blockQueue[0];
+				column = 3;
+				blockQueue.RemoveAt(0);
+				blockQueue.Add(blocks[Random.Range(0, 5)]);
+				timeRemaining = blockBuildTime;
+			}
+			if (Input.GetKeyUp (KeyCode.Alpha5) && timeRemaining <= 0 && !blockManager.ColFull(4))
+			{
+				currentlyBuilding = blockQueue[0];
+				column = 4;
+				blockQueue.RemoveAt(0);
+				blockQueue.Add(blocks[Random.Range(0, 5)]);
+				timeRemaining = blockBuildTime;
+			}
+		}
+
+		if (timeRemaining > 0) 
+		{
+			timeRemaining -= Time.deltaTime;
+		}
+		
+		if(timeRemaining <= 0 && currentlyBuilding != null)
+		{
+			blockManager.Build(currentlyBuilding, column);
+			currentlyBuilding = null;
+		}
+	}
+}
---
+++ Assets/Scripts/RTS/BlockManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockManager : MonoBehaviour {
+
+	public GameObject[,] existingBlocks = new GameObject[5,7];
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Build(GameObject block, int column)
+	{
+		int index = 0;
+
+		for(int i = 0; i < 7; i++)
+		{
+			if(existingBlocks[column, i] == null)
+			{
+				index = i;
+				break;
+			}
+		}
+
+		if(index == 0)
+		{
+			block.transform.position = new Vector3((column - 2) * 4, 1, (index - 2) * -4);
+			existingBlocks[column, 0] = GameObject.Instantiate(block);
+		}
+		else
+		{
+			for(int i = index; i > 0; i--)
+			{
+				existingBlocks[column, i - 1].transform.position = new Vector3((column - 2) * 4, 1, ((i - 2) * (-4)));
+				existingBlocks[column, i] = existingBlocks[column, i - 1];
+			}
+
+			block.transform.position = new Vector3((column - 2) * 4, 1, (0 - 2) * -4);
+			existingBlocks[column, 0] = GameObject.Instantiate(block);
+		}
+	}
+
+	public bool ColFull(int column)
+	{
+		for(int i = 0; i < 7; i++)
+		{
+			if(existingBlocks[column, i] == null)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+}
